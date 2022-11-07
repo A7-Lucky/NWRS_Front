06c0840634f1bd111735420cc3d677ce85041371
GyuhyeonKim <112394164+9yuhyeon@users.noreply.github.com>
2022-11-07 15:56:12 +0900

    fix : 이미지가 없는 경우 에러 발생 해결
    ### PR 타입(하나 이상의 PR 타입을 선택해주세요)
    - [ ] 디자인 변경
    - [ ]  기능 추가
    - [ ] 기능 삭제
    - [ ] 버그 수정
    - [ ] 의존성, 환경 변수, 빌드 관련 코드 업데이트
    ### 반영 브랜치
    - signup -> main
    ### 변경 사항
    - UserModel의 profile_img 필드에 null=True 추가
    - api.js에서 formData에 조건문으로 이미지가 있는 경우만 profile_img 추가(이미지가 없는 경우 formData에 append 할 시 파일이 없다는 에러가 발생) 
    ### 테스트 결과

--- static/js/api.js
+++ static/js/api.js
@@ -1,10 +1,8 @@
-window.onload = () => {
-    console.log('로딩 완료')
-}
 // 전역 변수 //
-const backend_base_url = 'http://127.0.0.1:8000/'
-const frontend_base_url = 'http://127.0.0.1:5500/'
+const backend_base_url = 'http://127.0.0.1:8000'
+const frontend_base_url = 'http://127.0.0.1:5500/templates'
 
+// 회원가입
 async function handleSignup() {
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
@@ -15,11 +13,15 @@ async function handleSignup() {
     const formData = new FormData()
     formData.append("username", username)
     formData.append("password", password)
-    formData.append("profile_img", profile_img)
+    
+    if (profile_img != undefined){
+        formData.append('profile_img', profile_img)
+    }
+    
     formData.append("introduce", introduce)
     formData.append("favorite", favorite)
     
-    const response = await fetch('http://127.0.0.1:8000/users/sign-up/', {
+    const response = await fetch(`${backend_base_url}/users/sign-up/`, {
         headers: {
         },
         method: 'POST',
@@ -29,6 +31,6 @@ async function handleSignup() {
 
     if (response.status == 201) {
         alert('가입 완료!')
-        window.location.replace(`${frontend_base_url}login.html`)
+        window.location.replace(`${frontend_base_url}/login.html`)
     }
 }
