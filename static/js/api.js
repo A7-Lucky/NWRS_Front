d5919e5deaa66f8e60e150170b90055edb193ff2
choding91 <whdlsrjf91@naver.com>
2022-11-08 09:14:39 +0900

    fix : 코드 에러 수정
    - 로그인, 회원가입 코드 정리
    - css 추가 및 수정, 삭제

--- static/js/api.js
+++ static/js/api.js
@@ -41,6 +41,49 @@ async function handleLogin() {
   }
 }
 
+// // login.html --------------------------------------------------------------------------->
+// // 로그인 //
+// async function handleLogin() {
+//   const LoginData = {
+//     username: document.getElementById("username").value,
+//     password: document.getElementById("password").value,
+//   };
+//   console.log(username, password);
+
+//   const response = await fetch(`${backend_base_url}/users/api/token/`, {
+//     headers: {
+//       Accept: "application/json",
+//       "content-type": "application/json",
+//     },
+//     method: "POST",
+//     body: JSON.stringify(LoginData),
+//   });
+
+//   response_json = await response.json();
+
+//   if (response.status == 200) {
+//     localStorage.setItem("access", response_json.access);
+//     localStorage.setItem("refresh", response_json.refresh);
+
+//     const base64Url = response_json.access.split(".")[1];
+//     const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+//     const jsonPayload = decodeURIComponent(
+//       atob(base64)
+//         .split("")
+//         .map(function (c) {
+//           return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+//         })
+//         .join("")
+//     );
+
+//     localStorage.setItem("payload", jsonPayload);
+
+//     window.location.replace(`${frontend_base_url}/index.html`);
+//   } else {
+//     alert("잘못된 로그인입니다.", response.status);
+//   }
+// }
+
 // 회원가입
 async function handleSignup() {
   const username = document.getElementById("username").value;
@@ -73,6 +116,33 @@ async function handleSignup() {
   }
 }
 
+// // signup.html --------------------------------------------------------------------------->
+// // 회원가입 //
+// async function handleSignup() {
+//   const SignupData = {
+//     username: document.getElementById("username").value,
+//     password: document.getElementById("password").value,
+//   };
+
+//   const response = await fetch(`${backend_base_url}/users/sign-up/`, {
+//     headers: {
+//       Accept: "application/json",
+//       "content-type": "application/json",
+//     },
+//     method: "POST",
+//     body: JSON.stringify(SignupData),
+//   });
+
+//   response_json = await response.json();
+
+//   if (response.status == 201) {
+//     window.location.replace(`${frontend_base_url}/login.html`);
+//   } else {
+//     // 공백에 대한 alert 추가 필요 //
+//     alert("이미 사용 중인 아이디입니다.");
+//   }
+// }
+
 // 공통 --------------------------------------------------------------------------->
 // 로그아웃 //
 function handleLogout() {
@@ -98,49 +168,6 @@ async function getWebtoons() {
 function webtoonDetail(webtoon_id) {
   const url = `${frontend_base_url}/webtoon_detail.html?id=${webtoon_id}`;
   location.href = url;
-}
-
-// login.html --------------------------------------------------------------------------->
-// 로그인 //
-async function handleLogin() {
-  const LoginData = {
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-  };
-  console.log(username, password);
-
-  const response = await fetch(`${backend_base_url}/users/api/token/`, {
-    headers: {
-      Accept: "application/json",
-      "content-type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(LoginData),
-  });
-
-  response_json = await response.json();
-
-  if (response.status == 200) {
-    localStorage.setItem("access", response_json.access);
-    localStorage.setItem("refresh", response_json.refresh);
-
-    const base64Url = response_json.access.split(".")[1];
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split("")
-        .map(function (c) {
-          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-
-    localStorage.setItem("payload", jsonPayload);
-
-    window.location.replace(`${frontend_base_url}/index.html`);
-  } else {
-    alert("잘못된 로그인입니다.", response.status);
-  }
 }
 
 // webtoon_detail.html --------------------------------------------------------------------------->
