34caaf19be58aa2e1de1b06057a9c909a10e9b90
9yuhyeon <kkh2742@gmail.com>
2022-11-08 10:49:28 +0900

    feat : 회원가입 버그 수정
    - 사용자 입력 값 정규표현식 검사 중 회원가입이 되는 버그 수정
    - signup.js에서 입력했던 정규표현식 검사 코드를 api.js의 handleSignup
      함수로 옮김

--- static/js/api.js
+++ static/js/api.js
@@ -1,55 +1,83 @@
-window.onload = () => {
-    console.log('로딩 완료')
-}
-// 전역 변수 //
-const backend_base_url = 'http://127.0.0.1:8000/'
-const frontend_base_url = 'http://127.0.0.1:5500/'
-
+// 회원가입
 async function handleSignup() {
-    const username = document.getElementById('username').value
-    const password = document.getElementById('password').value
-    const profile_img = document.getElementById('profile_img').files[0]
-    const introduce = document.getElementById('introduce').value
-    const favorite = document.getElementById('favorite').value
+    // 아이디 정규식 (4~12자의 영문 대소문자와 숫자)
+    var userCheck = RegExp(/^[a-zA-Z0-9]{4,12}$/);
+    // 패스워드 정규식 (영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자)
+    var passwdCheck = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{8,16}$/);
     
+    let username = document.querySelector('#username');
+    let password = document.querySelector('#password');
+    let password2 = document.querySelector('#password2');
+
+    // 아이디 유효성 검사
+    if (username.value == '') {
+        alert('아이디를 입력해주세요!')
+        username.focus();
+        return false;
+    }
+    else if(!userCheck.test(username.value)) {
+        alert("아이디는 4~12자의 영문 대소문자와 숫자로만 입력해주세요!");
+        username.focus();
+        return false;
+    }
+    
+    // 비밀번호 공백 검증
+    if (password.value == '') {
+        alert('비밀번호를 입력해주세요!')
+        password.focus();
+        return false;
+    }
+    // 비밀번호 유효성 검사
+    else if (!passwdCheck.test(password.value)) {
+        alert("비밀번호는 영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자로 입력해주세요!");
+        password.focus();
+        return false;
+    }
+    // 비밀번호 재확인 공백 검증
+    else if (password2.value == '') {
+        alert('비밀번호 재확인을 해주세요!')
+        password2.focus();
+        return false;
+    }
+    // 비밀번호와 비밀번호 재확인 일치 여부 검증
+    else if (password.value != password2.value) {
+        alert('비밀번호가 일치하지 않습니다!')
+        password2.focus();
+        return false;
+    }
+    // 아이디 비밀번호 값 중복 검증
+    else if (username.value == password.value) {
+        alert('아이디와 비밀번호는 같을 수 없습니다!')
+        password.focus();
+        return false;
+    }
+    const form_username = document.getElementById('username').value
+    const form_password = document.getElementById('password').value
+    const form_profile_img = document.getElementById('profile_img').files[0]
+    const form_introduce = document.getElementById('introduce').value
+    const form_favorite = document.getElementById('favorite').value
+
     const formData = new FormData()
-    formData.append("username", username)
-    formData.append("password", password)
-    formData.append("profile_img", profile_img)
-    formData.append("introduce", introduce)
-    formData.append("favorite", favorite)
+    formData.append("username", form_username)
+    formData.append("password", form_password)
+    if (profile_img != undefined) {
+    formData.append("profile_img", form_profile_img)
+    }
+    formData.append("introduce", form_introduce)
+    formData.append("favorite", form_favorite)
     
-    const response = await fetch('http://127.0.0.1:8000/users/sign-up/', {
-        headers: {
-        },
+    const response = await fetch(`${backend_base_url}/users/sign-up/`, {
+        headers: {},
         method: 'POST',
         body: formData
+        
     })
-    console.log(response)
-
+    
     if (response.status == 201) {
         alert('가입 완료!')
-        window.location.replace(`${frontend_base_url}login.html`)
+        window.location.replace(`${frontend_base_url}/login.html`)
+    } else if (response.status == 400){
+        alert('이미 가입된 유저입니다!')
+        window.location.reload()
     }
-}
-
-async function handleLogin() {
-    const username = document.getElementById('username').value
-    const password = document.getElementById('password').value
-    
-    const response = await fetch('http://127.0.0.1:8000/users/api/token/', {
-        headers: {
-            'content-type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify({
-            'username': username,
-            'password': password
-        })
-    })
-    const response_json = await response.json()
-    console.log(response_json)
-
-    localStorage.setItem('access', response_json.access);
-    localStorage.setItem('refresh', response_json.refresh);
 }
